5d66edecab583e58f977ebafad183197f2f937b4
Mstar <mjy3056@naver.com>
2023-06-13 04:04:22 +0900

    database setting

--- cmnai/config/settings.py
+++ cmnai/config/settings.py
@@ -74,9 +74,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default':{
+        'ENGINE': 'djongo',
+        'NAME': 'main_db',
+        'HOST': 'localhost',
+        'PORT': '27017'
     }
 }
 
