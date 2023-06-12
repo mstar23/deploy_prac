ce2af4621362828c3c87165950098aaab80ad8b2
Mstar <mjy3056@naver.com>
2023-06-13 05:30:37 +0900

    modify

--- cmnai/main/urls.py
+++ cmnai/main/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'main'
 
 urlpatterns = [
-    path('', views.first_page, name='first_page'),
+    # path('', views.first_page, name='first_page'),
+    path('main_page/', views.main_page, name='main_page'),
 ]
--- cmnai/main/views.py
+++ cmnai/main/views.py
@@ -3,5 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def first_page(request):
-    # return render(request, 'first_page.html')
-    return HttpResponse('안녕 테스트얌 ㅎㅎ')
+    return render(request, 'first_page.html')
+
+def main_page(request):
+    return render(request, 'main_page.html')
